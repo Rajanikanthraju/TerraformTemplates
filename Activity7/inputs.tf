70dbce081f816908b84e1f0a2d7f63543e5447e3
Rajanikanth <rajanikanthraju.r@gmail.com>
2022-06-06 16:26:19 +0530

    Added conditional  expressions

---
+++ Activity7/inputs.tf
@@ -0,0 +1,22 @@
+variable "region" {
+    type = string
+    default = "ap-southeast-1"
+    description = "target region where resource can be created"
+  
+}
+variable "vpc_cidr_range" {
+    type = string
+    default = "10.10.0.0/16"
+    description = "VPC cidr range"  
+}
+variable "name_tags" {
+    type= list(string)
+    default=["web1-tf","web2-tf","app1-tf","app2-tf","db1-tf","db2-tf"]
+  
+}
+variable "bucket" {
+    type= string
+    default="from-tf-test-bucket"
+    description = "bucket name"
+  
+}
